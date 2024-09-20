1fdef855b7e8e52f79f8b8bcfe46f7d16d8df5f7
Robert Bolender <robertbolender@github.com>
2024-09-20 14:28:27 -0700

    Remove comments

--- main.go
+++ main.go
@@ -13,17 +13,14 @@ type Repository struct {
 }
 
 func main() {
-  // Make a GET request to the GitHub API
   req, err := http.NewRequest("GET", fmt.Sprintf("https://api.github.com/users/%s/repos", "monalisa"), nil)
   if err != nil {
     fmt.Println("Failed to create request:", err)
     return
   }
 
-  // Add the Authorization header with the PAT
   req.Header.Add("Authorization", "token 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")
 
-  // Make the request using http.DefaultClient
   resp, err := http.DefaultClient.Do(req)
   if err != nil {
     fmt.Println("Failed to fetch repositories:", err)
@@ -31,7 +28,6 @@ func main() {
   }
   defer resp.Body.Close()
 
-  // Parse the response body into a slice of Repository structs
   var repositories []Repository
   err = json.NewDecoder(resp.Body).Decode(&repositories)
   if err != nil {
@@ -39,7 +35,6 @@ func main() {
     return
   }
 
-  // Print the details of each pinned repository
   for _, repo := range repositories {
     if repo.Description != "" {
       fmt.Printf("Name: %s\nDescription: %s\nLanguage: %s\n\n", repo.Name, repo.Description, repo.Language)
